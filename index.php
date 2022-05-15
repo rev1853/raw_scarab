58211fa9c6545889e1facd9162c1c66bfecc5e21
revvann <rghofur053@gmail.com>
2022-05-15 12:00:20 +0700

    change path

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST['path'])) {
     $path = $_POST['path'];
-    $content = file_get_contents($path);
+    $content = file_get_contents("raw/" . $path);
     echo json_encode(['content' => base64_encode($content)]);
 } else {
     echo "No path specified";
